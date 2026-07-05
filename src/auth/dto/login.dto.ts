4fe9a12752784f66b131d87e66c2bcdd489df38f
Yousef Hamed <yousefhamedhassan47@gmail.com>
2026-07-05 18:52:09 +0300

    Chore: modifed nest-cli.json

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -4,7 +4,6 @@ import { IsEmail, IsNotEmpty } from 'class-validator';
 export class LoginDto {
   @IsEmail()
   @IsNotEmpty()
-  // @ApiProperty({ example: 'test@gmail.com', description: 'the email' })
   email!: string;
   @IsNotEmpty()
   password!: string;
