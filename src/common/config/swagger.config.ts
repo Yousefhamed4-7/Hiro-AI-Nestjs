d4961263e5aa794878ee4d094b91076ddb78ae4f
Yousef Hamed <yousefhamedhassan47@gmail.com>
2026-07-05 18:29:27 +0300

    BugFix: Configured swagger to get his assets from his own public cdn

--- src/common/config/swagger.config.ts
+++ src/common/config/swagger.config.ts
@@ -32,3 +32,12 @@ export const swaggerConfig = new DocumentBuilder()
       enum: ['en', 'ar'], // Shows a dropdown list in the Swagger UI
     },
   });
+
+export const SWAGGER_CDN_OPTIONS = {
+  customCssUrl:
+    'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+  customJs: [
+    'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.js',
+  ],
+};
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,10 @@ import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerDocumentOptions, SwaggerModule } from '@nestjs/swagger';
-import { swaggerConfig } from './common/config/swagger.config';
+import {
+  SWAGGER_CDN_OPTIONS,
+  swaggerConfig,
+} from './common/config/swagger.config';
 import { TranslateResponseInterceptor } from './common/interceptors/translate-response.interceptor';
 import { TranslateExceptionFilter } from './common/filters/translate-exception.filter';
 
@@ -21,10 +24,11 @@ async function bootstrap() {
   const options: SwaggerDocumentOptions = {
     operationIdFactory: (controllerKey: string, methodKey: string) => methodKey,
   };
+
   const documentFactory = () =>
     SwaggerModule.createDocument(app, config, options);
 
-  SwaggerModule.setup('api', app, documentFactory);
+  SwaggerModule.setup('api', app, documentFactory, SWAGGER_CDN_OPTIONS);
 
   await app.listen(process.env.PORT ?? 3000);
 }
